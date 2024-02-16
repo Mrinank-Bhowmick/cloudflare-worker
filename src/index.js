bbd0b461321b076b8571a12322c78cf6a1f7571a
Mrinank-Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2024-02-16 19:32:08 +0530

    Initial commit (by create-cloudflare CLI)
    Details:
      C3 = create-cloudflare@2.11.3
      project name = webassembly
      package manager = npm@10.2.3
      wrangler = wrangler@3.28.1
      git = 2.37.3.windows.1

---
+++ src/index.js
@@ -0,0 +1,24 @@
+addEventListener('fetch', event => {
+	event.respondWith(handleRequest(event.request))
+  }) 
+  
+  async function handleRequest(request) {
+	const url = new URL(request.url)
+	if (url.pathname === '/') {
+	  return new Response('up and running', { status: 200 })
+	} else if (url.pathname === '/uuid' && request.method === 'GET') {
+	  return generateUUID()
+	} else {
+	  return new Response('Not found', { status: 404 })
+	}
+  }
+  
+  async function generateUUID() {
+	try {
+	  const uuid = crypto.randomUUID();
+	  return new Response(JSON.stringify({ uuid }), { status: 200, headers: { 'Content-Type': 'application/json' } });
+	} catch (err) {
+	  return new Response('Failed to generate UUID', { status: 500 });
+	}
+  }
+  
